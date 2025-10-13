4bb9d9cc0bbb2cca3cc602d46c63db64941a38c4
23emli04 <emilmoff@hotmail.com>
2025-10-13 11:49:39 +0200

    Var en liten bugg i förra committen.

--- src/features/sports/components/NavBar.jsx
+++ src/features/sports/components/NavBar.jsx
@@ -91,13 +91,47 @@ export default function Navbar() {
                                 </ul>
                             </details>
                         </li>
+                        <li className="dropdown">
+                            <label className="flex cursor-pointer gap-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <circle cx="12" cy="12" r="5"/>
+                                    <path
+                                        d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/>
+                                </svg>
+                                <input type="checkbox" value="synthwave" className="toggle theme-controller"/>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                                </svg>
+                            </label>
+                        </li>
+
                     </ul>
                 </div>
 
                 {/* Main menu (desktop) */}
 
 
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 lg:flex hidden">
                     {/* FOOTBALL */}
                     <li className="dropdown">
                         <details
@@ -175,15 +209,16 @@ export default function Navbar() {
                             </ul>
                         </details>
                     </li>
+
                 </ul>
             </div>
 
-            <div className="navbar-center hidden lg:flex">
+            <div className="navbar-center">
                 <a className="btn btn-ghost text-4xl">SportHub</a>
             </div>
 
             <div className="navbar-end">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 hidden lg:flex">
                     <li className="dropdown">
                         <label className="flex cursor-pointer gap-2">
                             <svg
