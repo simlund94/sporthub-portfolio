667894f0c69098a84dcf5b2188b5e1aa589a165d
23emli04 <emilmoff@hotmail.com>
2025-10-27 21:10:38 +0100

    Quality of life changes

--- src/features/sports/components/leaguePageComponents/SeasonSelector.jsx
+++ src/features/sports/components/leaguePageComponents/SeasonSelector.jsx
@@ -34,7 +34,7 @@ export default function SeasonSelector({leagueId, setLeagueId}) {
                          className="-mr-1 size-5 text-gray-400">
                         <path
                             d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
-                            clip-rule="evenodd" fill-rule="evenodd"/>
+                            />
                     </svg>
                 </summary>
                 <ul className="menu dropdown-content bg-base-200 shadow-sm w-52 p-2 ">
