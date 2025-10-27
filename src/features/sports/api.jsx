9557bb21eda3c6162a9107b70ee00bd0e67273a8
Simon Lundgren <sim.lund@outlook.com>
2025-10-27 09:31:32 +0100

    Ta bort oanvända api:er. Sortera api:er

--- src/features/sports/api.jsx
+++ src/features/sports/api.jsx
@@ -1,27 +1,23 @@
-import { api } from '../../lib/fetcher';
+import {api} from '../../lib/fetcher';
 
 export const SportsApi = {
-    sports: () => api('/sports'), // [ {id, name} ]
     events: (date, gender) => api(`/events?fromDate=${date}&teamClass=${gender}&toDate=${date}`),
     eventsById: (id) => api(`/events/${id}?fields=all`),
     eventsByTeam: (teamId, status) => api(`/events?limit=20&status=${status}&team=${teamId}&sort=startDate:desc`),
-    eventsByLeagueIdAndStatus: (id,status,order) => api(`/events?league=${id}&status=${status}&sort=startDate:${order}`),
+    eventsByLeagueIdAndStatus: (id, status, order) => api(`/events?league=${id}&status=${status}&sort=startDate:${order}`),
     allLeagues: () => api(`/leagues?limit=100`),
     leagueById: (id) => api(`/leagues/${id}`),
     leagueByIdWithEvents: (id, status, fromDate, toDate) => api(`/leagues/${id}/events?fields=all&status=${status}&fromDate=${fromDate}&toDate=${toDate}`),
     leagueByIdLastFiveGames: (id, status, teamId) => api(`/leagues/${id}/events?fields=all&status=${status}&team=${teamId}`),
-    leaguesByTeamId: (teamId) => api(`/leagues?${teamId}`),
     teamsByLeagueId: (leagueId) => api(`/leagues/${leagueId}/teams`),
     leaguesBySport: (sportId, query) => api(`/leagues?sport=${sportId}${query}`),
     leagueWithTeamsById: (leagueId) => api(`/leagues/${leagueId}/standings`),
     leagueAllSeasonsById: (leagueId) => api(`/leagues/${leagueId}/seasons`),
     leagueStandingsById: (leagueId) => api(`/leagues/${leagueId}/standings`),
+    leagueScoringLeadersById: (leagueId) => api(`/leagues/${leagueId}/scoring-leaders?limit=15`),
+    leagueAssistLeadersById: (leagueId) => api(`/leagues/${leagueId}/assist-leaders?limit=15`),
+    allLeaguesBySportAndGender: (sportId, gender) => api(`/leagues?sport=${sportId}&teamClass=${gender}&sort=startDate:desc`),
     teamById: (id) => api(`/teams/${id}`),
     teamStandings: (teamId) => api(`/teams/${teamId}/leagues?limit=1`),
-    allTeams: ()  => api(`/teams`),
-leagueScoringLeadersById:(leagueId) => api(`/leagues/${leagueId}/scoring-leaders?limit=15`),
-    leagueAssistLeadersById:(leagueId) => api(`/leagues/${leagueId}/assist-leaders?limit=15`),
-
-
-    allLeaguesBySportAndGender: (sportId, gender) => api(`/leagues?sport=${sportId}&teamClass=${gender}&sort=startDate:desc`),
+    allTeams: () => api(`/teams`),
 };
